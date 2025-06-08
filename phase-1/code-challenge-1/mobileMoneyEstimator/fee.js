2aeec3c96455fc586470957db0f52c430d0868de
Francis <franciskarani15@gmail.com>
2025-06-08 17:19:16 +0300

    adds validation for validity of the amount and the transaction costs

--- phase-1/code-challenge-1/mobileMoneyEstimator/fee.js
+++ phase-1/code-challenge-1/mobileMoneyEstimator/fee.js
@@ -1,16 +1,36 @@
 function estimateTransactionFee() {
-    const welcomeMessage =prompt("Unatuma ngapi?")
+    const welcomeMessage =prompt("Unatuma ngapi?(Enter amount in KSH)")
     const amountToSend =Number(welcomeMessage);
-    const minimumFee =10;
-    const maximumFee = 70;
-    // information about transactions and their fees
-    const basicTransactionFee = 1.5 * amountToSend / 100
+    const minimumFee =10; 
+    const maximumFee = 70; 
+   
 
-    // condition for the min and max fee
-    if (basicTransactionFee< minimumFee) {
+    // condition tommake sure the amount keyed in is a valid positive number
+    if (isNaN(amountToSend)|| amountToSend<=0) {
+        console.log("Oops! Please enter a valid number");
+        return;
         
     }
+     // information about transactions and their fees
+    const basicTransactionFee = 1.5 * amountToSend / 100
+    let finalFee;
+    // validation condition for the maximum and minimum amount to send.
+
+    if (basicTransactionFee < minimumFee) {
+        // charge ksh10 if the transaction fee is less than 10
+        let finalFee = minimumFee;
+        
+    } else if (basicTransactionFee>maximumFee) {
+        let finalFee = maximumFee;
+        // charge ksh70 if the transaction fee is more than 70        
+    } else{
+    // otherwise charge the transaction fee.
+    let finalFee =basicTransactionFee;}
 
 
+// variable fo the amount of money to be debited
+const amountToBeDebited = amountToSend + basicTransactionFee;
+// console output to the user
     
 }
+estimateTransactionFee()
