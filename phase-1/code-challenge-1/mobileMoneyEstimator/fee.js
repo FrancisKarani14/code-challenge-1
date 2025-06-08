19a0e0e2dbfdc19fcb6d9112252ec62bd4266119
Francis <franciskarani15@gmail.com>
2025-06-08 16:35:52 +0300

    adds prompt at the welcome message

--- phase-1/code-challenge-1/mobileMoneyEstimator/fee.js
+++ phase-1/code-challenge-1/mobileMoneyEstimator/fee.js
@@ -1,7 +1,16 @@
 function estimateTransactionFee() {
-    const welcomeMessage ="Unatuma ngapi?"
+    const welcomeMessage =prompt("Unatuma ngapi?")
     const amountToSend =Number(welcomeMessage);
-    
+    const minimumFee =10;
+    const maximumFee = 70;
+    // information about transactions and their fees
+    const basicTransactionFee = 1.5 * amountToSend / 100
+
+    // condition for the min and max fee
+    if (basicTransactionFee< minimumFee) {
+        
+    }
+
 
     
 }
