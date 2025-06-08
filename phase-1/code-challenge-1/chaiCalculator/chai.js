211fb4289383b9627701c6fdc7fca1e1e246ac0d
Francis <franciskarani15@gmail.com>
2025-06-08 11:28:13 +0300

    adds an argument to the number of cups

--- phase-1/code-challenge-1/chaiCalculator/chai.js
+++ phase-1/code-challenge-1/chaiCalculator/chai.js
@@ -1,6 +1,6 @@
 function calculateChaiIngredients() {
     const welcomeMessage = prompt("Karibu! How many cups of Chai Bora would you like to make?");
-    const numberOfcups = Number();
+    const numberOfcups = Number(welcomeMessage);
 
     // standard recipe for one cup of Chai
     const standardWaterInMl = 200;
@@ -9,10 +9,10 @@ function calculateChaiIngredients() {
     const standardSugarinTeaSpoon = 2;
   
     // calculate the number of specific ingredients based on the number of cups needed.
-    totalWaterInMl = numberOfcups*standardWaterInMl;
-    totalMilkInMl = numberOfcups*standardMilkInMl;
-    totalMajaniInTbsp = numberOfcups*standardMajaniInTbsp;
-    totalSugarInTeaSpoon =numberOfcups*standardSugarinTeaSpoon;
+    totalWaterInMl = numberOfcups * standardWaterInMl;
+    totalMilkInMl = numberOfcups * standardMilkInMl;
+    totalMajaniInTbsp = numberOfcups * standardMajaniInTbsp;
+    totalSugarInTeaSpoon =numberOfcups * standardSugarinTeaSpoon;
       // condition that makes sure the numbere of cups are valid
 
     if (isNaN(numberOfcups) || numberOfcups <= 0) {
